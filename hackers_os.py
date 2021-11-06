28ff6dab10e3912349739ea52679a8dccacebe86
KrouZ_CZ <64690903+KvantPro@users.noreply.github.com>
2021-11-06 15:59:43 +0300

    Update hackers_os.py

--- hackers_os.py
+++ hackers_os.py
@@ -1,11 +1,12 @@
 from os import system as cmd
 from progress.bar import IncrementalBar
+from consolemenu import SelectionMenu
 import os
 import random
 import time
 import hashlib
 
-cmd('mode con: cols=80 lines=20')
+cmd('mode con: cols=80 lines=21')
 cmd('title HACKERS OS')
 if cmd('cd data') != 0:
 	cmd('mkdir data')
@@ -19,7 +20,7 @@ def logo():
 			file.close()
 			cmd('cls')
 			print('')
-			print('              HACKER OS 1.4v')
+			print('              HACKER OS 2.0v')
 			print('              Enter password')
 			pas = input('              >')
 			hashs = hashlib.md5(pas.encode())
@@ -38,23 +39,31 @@ def logo():
 
 def select():
 	cmd('cls')
-	print('Select to hack:')
-	print('1. Gmail\n2. Vk\n3. Telegram\n4. What`s App\n5. Discord\n6. Minecraft\n7. Steam')
+	a_list=['Gmail','Vk','Telegram','What`s App','Discord','Minecraft','Steam','Geometry dash','Logs']
+	menu = SelectionMenu(a_list,"Select to hack")
+	menu.show()
+	menu.join()
+	selection = menu.selected_option
+	return selection
 def goto(sel):
-	if int(sel) == 1:
+	if int(sel) == 0:
 		Gmail()
-	if int(sel) == 2:
+	if int(sel) == 1:
 		Vk()
-	if int(sel) == 3:
+	if int(sel) == 2:
 		Tl()
-	if int(sel) == 4:
+	if int(sel) == 3:
 		Wa()
-	if int(sel) == 5:
+	if int(sel) == 4:
 		DI()
-	if int(sel) == 6:
+	if int(sel) == 5:
 		MC()
-	if int(sel) == 7:
+	if int(sel) == 6:
 		St()
+	if int(sel) == 7:
+		GD()
+	if int(sel) == 8:
+		log()
 def Gmail():
 	cmd('cls')
 	mail = input('Enter gmail: ')
@@ -64,7 +73,7 @@ def Gmail():
 	print('[+] Connecting to --> gmail.com')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Hacking servers', max=100, suffix='%(percent)d%%')
-	for i in range(100):
+	for i in range(random.randint(1, 100)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -80,7 +89,7 @@ def Gmail():
 	print('[+] Data detected')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Decrypting the password', max=200, suffix='%(percent)d%%')
-	for i in range(200):
+	for i in range(random.randint(1, 200)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -88,7 +97,7 @@ def Gmail():
 	print('[+] Password decrypted')
 	time.sleep(0.5)
 	print('')
-	c = '[HACKED]    ' + mail + '@gmail.com' + passgen('abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 8, 12)
+	c = '[HACKED/Gmail] ' + mail + '@gmail.com' + passgen('abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 8, 12)
 	print(c)
 	print('')
 	cmd('echo ' + c + ' >> data\\log.txt')
@@ -103,15 +112,15 @@ def Vk():
 	print('[+] Connecting to --> vk.com')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Hacking servers', max=120, suffix='%(percent)d%%')
-	for i in range(120):
+	for i in range(random.randint(1, 120)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
 	time.sleep(0.5)
 	print('[+] Servers has hacked')
 	time.sleep(0.5)
-	bar = IncrementalBar('[*] Search code of vk', max=80, suffix='%(percent)d%%')
-	for i in range(80):
+	bar = IncrementalBar('[*] Search code of vk', max=100, suffix='%(percent)d%%')
+	for i in range(random.randint(1, 100)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -127,7 +136,7 @@ def Vk():
 	print('[+] Code decrypted')
 	time.sleep(0.5)
 	print('')
-	c = '[HACKED]    ' + nu + passgen('0123456789', 4, 4)
+	c = '[HACKED/VK]   ' + nu + passgen('0123456789', 4, 4)
 	print(c)
 	print('')
 	cmd('echo ' + c + ' >> data\\log.txt')
@@ -142,7 +151,7 @@ def Tl():
 	print('[+] Connecting to --> telegram')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Hacking servers', max=250, suffix='%(percent)d%%')
-	for i in range(250):
+	for i in range(random.randint(1, 250)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -166,7 +175,7 @@ def Tl():
 	print('[+] Code decrypted')
 	time.sleep(0.5)
 	print('')
-	c = '[HACKED]    ' + nu + passgen('0123456789', 5, 5)
+	c = '[HACKED/Teleg]  ' + nu + passgen('0123456789', 5, 5)
 	print(c)
 	print('')
 	cmd('echo ' + c + ' >> data\\log.txt')
@@ -181,7 +190,7 @@ def Wa():
 	print('[+] Connecting to --> What`s App')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Hacking servers', max=20, suffix='%(percent)d%%')
-	for i in range(20):
+	for i in range(random.randint(1, 20)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -189,7 +198,7 @@ def Wa():
 	print('[+] Servers has hacked')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Search code of WA', max=50, suffix='%(percent)d%%')
-	for i in range(50):
+	for i in range(random.randint(1, 50)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -205,7 +214,7 @@ def Wa():
 	print('[+] Code decrypted')
 	time.sleep(0.5)
 	print('')
-	c = '[HACKED]    ' + nu + passgen('0123456789', 6, 6)
+	c = '[HACKED/WA]  ' + nu + passgen('0123456789', 6, 6)
 	print(c)
 	print('')
 	cmd('echo ' + c + ' >> data\\log.txt')
@@ -219,7 +228,7 @@ def DI():
 	print('[+] Connecting to --> gmail.com')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Hacking servers', max=100, suffix='%(percent)d%%')
-	for i in range(100):
+	for i in range(random.randint(1, 100)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -243,7 +252,7 @@ def DI():
 	print('[+] Code decrypted')
 	time.sleep(0.5)
 	print('')
-	c = '[HACKED]    ' + gmail + '@gmail.com' + passgen('0123456789', 6, 6)
+	c = '[HACKED/DS]  ' + gmail + '@gmail.com' + passgen('0123456789', 6, 6)
 	print(c)
 	print('')
 	cmd('echo ' + c + ' >> data\\log.txt')
@@ -257,7 +266,7 @@ def MC():
 	print('[+] Connecting to --> minecraft.net')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Hacking servers', max=150, suffix='%(percent)d%%')
-	for i in range(150):
+	for i in range(random.randint(1, 150)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -297,7 +306,7 @@ def MC():
 	print('[+] Code decrypted')
 	time.sleep(0.5)
 	print('')
-	c = '[HACKED]    ' + nick + passgen('abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 8, 12)
+	c = '[HACKED/MC]  ' + nick + passgen('abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 8, 12)
 	print(c)
 	print('')
 	cmd('echo ' + c + ' >> data\\log.txt')
@@ -311,15 +320,15 @@ def St():
 	print('[+] Connecting to --> steam.com')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Hacking servers', max=150, suffix='%(percent)d%%')
-	for i in range(150):
+	for i in range(random.randint(1, 150)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
 	time.sleep(0.5)
 	print('[+] Servers has hacked')
 	time.sleep(0.5)
-	bar = IncrementalBar('[*] Search  Steam name of nick', max=40, suffix='%(percent)d%%')
-	for i in range(40):
+	bar = IncrementalBar('[*] Search  Steam name of nick', max=100, suffix='%(percent)d%%')
+	for i in range(random.randint(1, 100)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -327,7 +336,7 @@ def St():
 	print('[+] Name detected')
 	time.sleep(0.5)
 	bar = IncrementalBar('[*] Hacking password', max=100, suffix='%(percent)d%%')
-	for i in range(100):
+	for i in range(random.randint(1, 100)):
 		bar.next()
 		time.sleep(0.1)
 	bar.finish()
@@ -335,10 +344,74 @@ def St():
 	print('[+] Password hacked')
 	time.sleep(0.5)
 	print('')
-	c = '[HACKED]    __' + nick + '__' + passgen('abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 8, 12)
+	c = '[HACKED/Steam] __' + nick + '__' + passgen('abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 8, 12)
 	print(c)
 	print('')
 	cmd('echo ' + c + ' >> data\\log.txt')
+	input('To return, press enter')
+def GD():
+	cmd('cls')
+	nick = input('Enter geometry dash nick: ')
+	a_list=['Moderator', 'Elder moderator', 'Account']
+	menu = SelectionMenu(a_list,"Select to hack")
+	menu.show()
+	menu.join()
+	select = menu.selected_option
+	time.sleep(0.5)
+	print('Nick to hack --> ' + nick)
+	time.sleep(0.3)
+	print('[+] Connecting to --> geometry dash')
+	time.sleep(0.5)
+	bar = IncrementalBar('[*] Hacking servers', max=50, suffix='%(percent)d%%')
+	for i in range(random.randint(1, 50)):
+		bar.next()
+		time.sleep(0.1)
+	bar.finish()
+	time.sleep(0.5)
+	print('[+] Servers has hacked')
+	time.sleep(0.5)
+	if select == 0:
+		bar = IncrementalBar('[*] Gives moderator', max=20, suffix='%(percent)d%%')
+		for i in range(20):
+			bar.next()
+			time.sleep(0.1)
+		bar.finish()
+		time.sleep(0.5)
+		print('[+] Moderator gives')
+		time.sleep(0.5)
+	elif select == 1:
+		bar = IncrementalBar('[*] Gives elder moderator', max=20, suffix='%(percent)d%%')
+		for i in range(20):
+			bar.next()
+			time.sleep(0.1)
+		bar.finish()
+		time.sleep(0.5)
+		print('[+] Elder Moderator gives')
+		time.sleep(0.5)
+	elif select == 2:
+		bar = IncrementalBar('[*] Hacking password', max=100, suffix='%(percent)d%%')
+		for i in range(random.randint(1, 100)):
+			bar.next()
+			time.sleep(0.1)
+		bar.finish()
+		time.sleep(0.5)
+		print('[+] Password hacked')
+		time.sleep(0.5)
+	print('')
+	if select == 0:
+		c = '[HACKED/GD]  ' + nick + ' moderator gives'
+	elif select == 1:
+		c = '[HACKED/GD]  ' + nick + ' elder moderator gives'
+	elif select == 2:
+		c = '[HACKED/GD]  ' + nick + passgen('abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890', 8, 12)
+	print(c)
+	print('')
+	cmd('echo ' + c + ' >> data\\log.txt')
+	input('To return, press enter')
+def log():
+	file = open('data\\log.txt', 'r')
+	print(file.read())
+	file.close()
 	input('To return, press enter')
 def passgen(chars, min, max):
 	length = random.randint(min, max)
@@ -349,6 +422,4 @@ def passgen(chars, min, max):
 if __name__ == '__main__':
 	logo()
 	while True:
-		select()
-		sel = input('>')
-		goto(sel)
+		goto(select())
